76476ae4e3930f73e9011f52e23c5af0566d7749
Neeraj <95131785+Neerajred@users.noreply.github.com>
2024-12-20 17:20:25 +0530

    updated path

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Navigate to Odoo directory
-cd /path/to/your/odoo
+cd /opt/odoo/odoo/custom_modules/muqeed_property
 
 # Pull latest code from the specified branch
 git pull origin master
